6d3ca265667d1b1f67bbdecf3c8fe095b1640123
jasmin-guven <jasmin.guven@hotmail.com>
2022-10-03 14:44:03 +0100

    pull code

--- scripts/plot_em.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import MDAnalysis as mda 
-import MDAnalysis.auxiliary 
-
-
-potential_file = "../potential.xvg"
-reader = mda.auxiliary.XVG.XVGReader(potential_file)
-print(reader)
